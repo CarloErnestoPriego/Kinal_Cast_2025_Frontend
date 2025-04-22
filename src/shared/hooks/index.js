7d9d4ade411256a03b1d5ee34680dff094bb1484
CarloErnestoPriego <cpriego-2020544@kinal.edu.gt>
2025-04-22 11:49:40 -0600

    Proyecto Casi Terminado

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -1,6 +1,10 @@
+import { useFollowChannel } from "./useFollowChannel";
+
 export { useLogin } from "./useLogin";
 export { useRegister } from "./useRegister";
 export { logout } from './userLogout';
 export { useUserDetails} from './useUserDetails';
 export { useChannelSettings } from './useChangeSettings'
 export { useChangePassword } from './useChangePassword'
+export { useFollowChannel } from './useFollowChannel'
+export { useChannelDetails } from './useChannelDetails'
